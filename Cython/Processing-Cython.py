8016b4a96c04638d263351c578ec9c2c49997ab5
wurdahl <wurdahl@gmail.com>
2021-10-18 17:08:42 -0500

    finishing up Memory

--- Cython/Processing-Cython.py
+++ Cython/Processing-Cython.py
@@ -25,7 +25,7 @@ npix = hp.nside2npix(nside)
 
 
 rangeOfInterest=1024#only look at particles within this
-radialDivs = 8
+radialDivs = 256
 ROIs = np.linspace(0,rangeOfInterest, radialDivs+1)
 
 boxSize=2048 # side length of box
